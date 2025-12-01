0cb12d1e63cb3be4c798f7bdaf3e76d32ecd9536
Richard Sam Jackson <info.rsamjackson@gmail.com>
2025-12-01 11:55:36 +0000

    Updating api views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from .models import Book
 from .serializers import BookSerializer
 
@@ -6,3 +6,12 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    # Add ordering support
+    filter_backends = [filters.OrderingFilter]
+
+    # Optional: specify fields you want to allow ordering by
+    ordering_fields = ['title', 'author']
+
+    # Optional default ordering
+    ordering = ['title']
