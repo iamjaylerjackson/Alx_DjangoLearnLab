3625449ae10d3e3ba4d81e6f0883ad70b5acb270
Richard Sam Jackson <info.rsamjackson@gmail.com>
2025-12-14 17:09:34 +0000

    posts/views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -5,6 +5,10 @@ from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
 
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
+
 
 class PostPagination(PageNumberPagination):
     page_size = 10
@@ -30,3 +34,19 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # users the current user is following
+        following_users = user.following.all()
+
+        # posts from followed users
+        return Post.objects.filter(
+            author__in=following_users
+        ).order_by('-created_at')
